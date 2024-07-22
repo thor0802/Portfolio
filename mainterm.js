987bcbfcd6fad9972dd1cb5139272326011779a4
thor0802 <nishantsinha0808@gmail.com>
2024-07-22 16:23:56 +0530

    updated

---
+++ mainterm.js
@@ -0,0 +1,7 @@
+var Typed = new Typed(".text", {
+  strings: ["Web Developer", "Python Developer", "Frontend Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
