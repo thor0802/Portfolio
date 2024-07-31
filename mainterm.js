5e47a22c9ddc70f49bef1e83b99fab125bd5df71
thor0802 <nishantsinha0808@gmail.com>
2024-08-01 00:16:56 +0530

    update

--- mainterm.js
+++ mainterm.js
@@ -5,3 +5,24 @@ var Typed = new Typed(".text", {
   backDelay: 1000,
   loop: true
 });
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
